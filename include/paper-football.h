8fd3543b94b86106437040a276d37103c4fcf965
Andrii Sevastianov <mustitz@gmail.com>
2019-05-03 10:00:23 +0300

    Add readline loop.
    Any command is treated as “quit”.

--- include/paper-football.h
+++ include/paper-football.h
@@ -1,4 +1,6 @@
 #ifndef YOO__PAPER_FOOTBALL__H__
 #define YOO__PAPER_FOOTBALL__H__
 
+#include <stdlib.h>
+
 #endif
--- sources/main.c
+++ sources/main.c
@@ -1,6 +1,31 @@
 #include "paper-football.h"
 
+#include <stdio.h>
+
+int process_cmd(const char * const line)
+{
+    return 1;
+}
+
 int main()
 {
+    char * line = 0;
+    size_t len = 0;
+    for (;; ) {
+        const ssize_t has_read = getline(&line, &len, stdin);
+        if (has_read == -1) {
+            break;
+        }
+
+        const int is_quit = process_cmd(line);
+        if (is_quit) {
+            break;
+        }
+    }
+
+    if (line) {
+        free(line);
+    }
+
     return 0;
 }
