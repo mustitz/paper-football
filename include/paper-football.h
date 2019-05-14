462f487f9a0ca9d33885987f8becb5a908f43875
Andrii Sevastianov <mustitz@gmail.com>
2019-05-14 13:57:19 +0300

    Add board geometry functionality.
    Create geometry struct to get next square after moving in given direction.
    Special negative constants are used:
        NO_WAY: it is impossible to move in given direction;
        GOAL_1:  game over, player 1 wins;
        GOAL_2:  game over, player 2 wins.

--- include/paper-football.h
+++ include/paper-football.h
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 
 void * multialloc(
@@ -11,5 +12,38 @@ void * multialloc(
     const size_t * const sizes,
     void * restrict * ptrs,
     const size_t granularity);
+
+
+
+#define GOAL_1   -1
+#define GOAL_2   -2
+#define NO_WAY   -3
+
+enum step {
+    NORTH_WEST = 0,
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+    QSTEPS
+};
+
+
+
+struct geometry
+{
+    uint32_t qpoints;
+    const int32_t * connections;
+};
+
+struct geometry * create_std_geometry(
+    const int width,
+    const int height,
+    const int goal_width);
+
+void destroy_geometry(struct geometry * restrict const me);
 
 #endif
